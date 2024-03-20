e34912f709dd531af9c7b278ac8ef9c75573bc8d
NachoMolina <juanignaciomolina12@gmail.com>
2024-03-20 20:08:37 -0300

    agregados test de carrito compra

--- challenge-testing/test/CarritoCompra.test.js
+++ challenge-testing/test/CarritoCompra.test.js
@@ -7,13 +7,26 @@ describe('Funcionalidad de CarritoCompra', () =>{
     let carrito;
     beforeEach(() => {
         carrito = new CarritoCompra
-    })
+    });
     
-    it('',()=>{
+    it('Deberia ser una instancia',()=>{
         expect(carrito instanceof CarritoCompra).toBe(true);
-    })
+    });
 
-    it("",()=>{
+    it("Deberia ser una clase",()=>{
         expect(typeof CarritoCompra.prototype.constructor).toBe("function");
-    })
+    });
+
+    it("Deberia tener un metodo llamado 'agregarProducto'",()=>{
+        expect(typeof carrito.agregarProducto).toBe('function');
+    });
+
+    it("Deberia tener un metodo llamado 'calcularTotal'",()=>{
+        expect(typeof carrito.calcularTotal).toBe('function');
+    });
+
+    it("Deberia tener un metodo llamado 'aplicarDescuento'",()=>{
+        expect(typeof carrito.aplicarDescuento).toBe('function');
+
+    });
 });
